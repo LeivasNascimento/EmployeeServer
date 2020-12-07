615ddee8642c0b0086ad92675d929deddb31df78
Leivas Nascimento Dos Santos <lnsantos2@latam.stefanini.com>
2020-12-07 11:27:35 -0300

    Adicionar arquivos de projeto.

---
+++ EmployeeServer/Context/EmployeeContext.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeServer.Context
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+            /*
+             * Both EmployeeContext and DbContext have a constructor. The next statement is according to this situation:
+             * In this case, both the base class and derived class has their own constructors, so the process is complicated 
+             * because the constructors of both classes must be executed. 
+             * To overcome this situation C# provide a keyword known as a base keyword. With the help of base keyword, 
+             * the derived class (EmployeeContext) can call the constructor which is defined in its base class (DbContext).
+             * source: https://www.geeksforgeeks.org/c-sharp-inheritance-in-constructors/
+             * 
+             * */
+
+
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeServer/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+//https://www.c-sharpcorner.com/article/build-crud-operation-using-angular-9-and-dot-net-core-3-1-webapi-and-deploy-to-a/
+namespace EmployeeServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
